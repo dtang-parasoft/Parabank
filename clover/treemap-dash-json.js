e17580caed439ccd2c8c1b75b8dcdb026b003d89
dtang <dtang@parasoft.com>
2023-10-16 14:31:58 +0800

    11111

--- clover/treemap-dash-json.js
+++
@@ -1,9 +0,0 @@
-var treeMapJson = {"id":"Clover database 星期三 五月 10 2023 11:13:41 CST0","name":"","data":{
-    "$area":12.0,"$color":50.0,"title":" 12 Elements, 50% Coverage"},
-  "children":[{"id":"com.cenqua.clovertest.subpackage0","name":
-      "com.cenqua.clovertest.subpackage","data":{"$area":12.0,"$color":
-        50.0,"title":
-        "com.cenqua.clovertest.subpackage 12 Elements, 50% Coverage"},
-      "children":[]}]}
-;
-processTreeMapDashJson (treeMapJson);
